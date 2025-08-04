5cfd1e5f42af573ee55a616b4093723d764f8b20
Aarishbangash <157419986+Aarishbangash@users.noreply.github.com>
2025-08-05 01:03:58 +0500

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 
 # Load model and encoders
 try:
-    with open("model.pkl", "rb") as f:
+    with open("heart_model.pkl", "rb") as f:
         model = pickle.load(f)
 
     with open("scaler.pkl", "rb") as f:
